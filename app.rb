ba10d3178e10654eae30f7aeeb3c77cb291c08d2
Laurie <Laurie.Annie.Clemence@gmail.com>
2018-10-19 01:23:30 +0200

    done done

--- app.rb
+++ app.rb
@@ -1,36 +1,12 @@
-=begin
-require 'bundler'
-Bundler.require
+#$:.unshift File.expand_path("./../lib", __FILE__)
 
-$:.unshift File.expand_path("./../lib", __FILE__)
 
-Done.new.perform
-Index.new.perform
-
-#require 'bundler'
-#Bundler.require
-require './lib/app/townhalls_scrapper.rb'
-require 'json'
-#require 'townhallsScrapper'
-
-class TownhallsAdderToDb < TownhallsScrapper
-
-  def save_into_json(hash)
-    File.open("./db/townhalls.json", 'w') do |f|
-      f.write(hash.to_json)
-    end
-    puts "JSON file saved"
-  end
-
-  def perform
-    save_into_json(TownhallsScrapper.new.perform)
-  end
-end
-
-TownhallsAdderToDb.new.perform
-=end
 require './lib/app/townhalls_scrapper.rb'
 require 'json'
 require './lib/app/townhalls_adder_to_db.rb'
+require './lib/views/done.rb'
+require './lib/views/index.rb'
 
-TownhallsAdderToDb.new("./db/townhalls.json").perform
+
+Done.new("./db/townhalls.json").perform
+Index.new.perform

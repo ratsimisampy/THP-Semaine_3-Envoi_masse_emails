0d3fa179bac55a17f3871ee3d39013c8167e70a2
Janus7593 <soparisbandit@gmail.com>
2018-10-18 21:04:39 +0200

    bot twitter

--- lib/app/townhalls_follower.rb
+++ lib/app/townhalls_follower.rb
@@ -1,3 +1,49 @@
-class TownhallsFollower
+require 'twitter'
+require 'twitter/search_results'
+require 'json'
+require 'dotenv'
 
-end
+Dotenv.load
+
+class BotTwitter
+
+	def perform
+
+		initialize
+
+	end
+
+	def initialize
+
+
+		@client = Twitter::REST::Client.new do |config|
+ 			config.consumer_key        = ENV["TWITTER_API_KEY"]
+  			config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+  			config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  			config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+		end
+
+		    = JSON.parse(File.read("./db/townhalls.json"))
+
+		 search_handle
+		# follow_handle
+	end
+
+	def search_handle
+		# journalistes = ["@Montrouge"]
+		    .each do |handle| 
+		    @temp = @client.user_search(handle)[0]
+			puts @temp.screen_name  
+			follow_handle
+		end
+	end
+	
+	def follow_handle
+		@client.follow (@temp)	
+	end
+end	
+
+
+
+test = BotTwitter.new.perform
+

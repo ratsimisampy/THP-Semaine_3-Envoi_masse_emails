eb183ae884fe03d181b6d829ee16e881f2071b24
Laurie <Laurie.Annie.Clemence@gmail.com>
2018-10-19 03:10:09 +0200

    require en attente

--- app.rb
+++ app.rb
@@ -1,17 +1,23 @@
+=begin
+EN ATTENTE :
+#require './lib/app/townhalls_follower.rb'
+#require './lib/app/townhalls_mailer.rb'
+#require 'mime'
+#include MIME
+#require 'twitter'
+#require 'twitter/search_results'
+
+=end
+
 require './lib/app/townhalls_scrapper.rb'
 require 'json'
 require './lib/views/done.rb'
 require './lib/app/townhalls_adder_to_db.rb'
-require './lib/app/townhalls_follower.rb'
 require './lib/views/index.rb'
 require 'google/apis/gmail_v1'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'base64'
-require 'mime'
-include MIME
-require 'twitter'
-require 'twitter/search_results'
 require 'json'
 require 'dotenv'
 Dotenv.load
